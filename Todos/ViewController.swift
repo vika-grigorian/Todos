00d09ca8aa4d423ac2d387f260943ade0c042c72
Vika <vg.dev31@gmail.com>
2025-02-21 11:48:49 +0400

    Initial Commit

---
+++ Todos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Todos
+//
+//  Created by Vika on 21.02.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
