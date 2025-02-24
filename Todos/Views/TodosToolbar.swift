1077a7e581d7c7a4873efed006302452849b1cb0
Vika <vg.dev31@gmail.com>
2025-02-24 21:27:38 +0400

    fix toolbar

--- Todos/Views/TodosToolbar.swift
+++ Todos/Views/TodosToolbar.swift
@@ -31,27 +31,43 @@ class TodosToolbar: UIToolbar {
         countLabel.textAlignment = .center
         countLabel.textColor = .label
         countLabel.font = UIFont.systemFont(ofSize: 16)
-        addButton.tintColor = .systemYellow
-
-//        countLabel.sizeToFit()
         
-        let countItem = UIBarButtonItem(customView: countLabel)
+        addButton.tintColor = .systemYellow
+        
+        // Создаем StackView для правильного выравнивания
+        let stackView = UIStackView(arrangedSubviews: [countLabel])
+        stackView.axis = .horizontal
+        stackView.alignment = .center
+        stackView.spacing = 8
+        
+        // Создаем UIBarButtonItem с кастомным view
+        let countItem = UIBarButtonItem(customView: stackView)
         let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
-        setItems([countItem, flexibleSpace, addButton], animated: false)
-    }
-    
-    private func configureConstraints() {
-            translatesAutoresizingMaskIntoConstraints = false
+        
+        setItems([flexibleSpace, countItem, flexibleSpace, addButton], animated: false)
     }
     
     // MARK: - Публичные методы
     func updateCount(_ count: Int) {
-        let formattedCount = String(count)
-        countLabel.text = "\(formattedCount) задач"
+//        let formattedCount = String(count)
+        countLabel.text = tasksString(for: count)
+
+//        countLabel.text = "\(formattedCount) задач"
     }
     
     func setAddTarget(_ target: Any?, action: Selector) {
 //        addButton.target = target
         addButton.action = action
+    }
+    
+    func tasksString(for count: Int) -> String {
+        switch count % 10 {
+        case 1 where count % 100 != 11:
+            return "\(count) задача"
+        case 2...4 where (count % 100 < 10) || (count % 100 >= 20):
+            return "\(count) задачи"
+        default:
+            return "\(count) задач"
+        }
     }
 }
